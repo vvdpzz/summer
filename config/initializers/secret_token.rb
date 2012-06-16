c204869c55c9c1c70d9c32c8bda0629e91c0509b
vvdpzz <vvdpzz@gmail.com>
2012-06-16 17:35:11 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Summer::Application.config.secret_token = 'fbd8ef21d2126cd55e9b09cec6c1229d4fec10f38078317f0ba4765b93dc1c4861eff93ddf3fd87dcedcd4c3b5d34f47c1d3f12d1beac1d96558a6b16d64e062'
