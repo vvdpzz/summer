aa9d9c78641064811887b77a3203f87af6c4b92e
vvdpzz <vvdpzz@gmail.com>
2012-06-18 15:14:17 +0800

    unique list name

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ActiveRecord::Base
   attr_accessible :category_id, :compiled, :cover, :name
-  
+  validates_uniqueness_of :name, :on => :create, :message => "must be unique"
   mount_uploader :cover, PhotoUploader
 end
