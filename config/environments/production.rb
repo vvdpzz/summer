9686611be8ecdb9a3ecd943577833b2b590de8a7
xxd <xuexiaodong79@gmail.com>
2012-06-18 14:58:32 +0800

    config.assets.compile = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,14 +15,14 @@ Summer::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
 
   # Using yui to compress js
   config.assets.js_compressor = :uglifier
-  
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
